057c51f5e90c0ad058b7a14d7815949430210fec
MaheshBhairewar <maheshbhairewar@gmail.com>
2023-06-16 19:13:43 +0530

    initial commit

---
+++ src/main/java/com/nt/source/Arithmetic.java
@@ -0,0 +1,12 @@
+package com.nt.source;
+
+public class Arithmetic{
+
+	public int add(int a,int b){
+		return a+b;}
+public static void main(String args[]){
+
+	Arithmetic ar = new Arithmetic();
+	System.out.println("sum is: "+ar.add(10,30));
+}
+}
